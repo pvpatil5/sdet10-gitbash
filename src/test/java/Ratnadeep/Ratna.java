3a20c391d695447fd595d4201b5a8b07e3e02c0b
ratnadeep <ratnadeepkhaple5@gmail.com>
2022-11-11 17:19:44 +0530

    commit from ratna

---
+++ src/test/java/Ratnadeep/Ratna.java
@@ -0,0 +1,11 @@
+package Ratnadeep;
+
+public class Ratna {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("code by Ratna-Mahesh");	
+	}
+
+}
